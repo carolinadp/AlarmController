7a4ad737c313c462a2304e7d9854bb4e31eddba2
Itzel Carolina Delgadillo <caroagsmex@hotmail.com>
2017-12-03 15:47:45 -0600

    Added select device screen

---
+++ app/src/main/java/edu/up/alarmcontroller/AlarmControlActivity.java
@@ -0,0 +1,13 @@
+package edu.up.alarmcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_control);
+    }
+}
